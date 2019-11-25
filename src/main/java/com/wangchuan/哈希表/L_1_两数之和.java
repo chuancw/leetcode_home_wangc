043b82b05aa26e6179170325339391161391c22a
chuancw <2402873932@qq.com>
2019-11-25 23:26:49 +0800

    DP

---
+++ src/main/java/com/wangchuan/哈希表/L_1_两数之和.java
@@ -0,0 +1,23 @@
+package com.wangchuan.哈希表;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，
+ * 请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ */
+public class L_1_两数之和 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int other = target - nums[i];
+            if (map.containsKey(other)) {
+                return new int[]{map.get(other), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
